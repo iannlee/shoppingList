c8f749e9325fb0b2ff4edeb88dc89cf6315fb663
Ian Lee <ianlee@jh-mba158-15.local>
2019-02-14 14:50:34 -0600

    added second view controller and file

---
+++ shoppingList/Item.swift
@@ -0,0 +1,22 @@
+//
+//  Item.swift
+//  shoppingList
+//
+//  Created by Ian Lee on 2/12/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var name:String
+    var quantity:Int
+    init(name:String) {
+        self.name = name
+        self.quantity = 1
+    }
+    init(name:String, quantity:Int) {
+        self.name = name
+        self.quantity = quantity
+    }
+    
+}
--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -14,20 +14,47 @@ class ViewController: UIViewController, UITableViewDataSource {
   
     @IBOutlet weak var tableView: UITableView!
     
+    var foodNeeds:[Item] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        
+        let itemOne = Item(name: "Cheese")
+        let itemTwo = Item(name: "Carrots", quantity: 12)
+        let itemThree = Item(name: "Apples", quantity: 4)
+        
+        
+        foodNeeds = [itemOne, itemTwo, itemThree]
+
     }
 
     @IBAction func addItemButtonPressed(_ sender: Any) {
-        
+        if let newItemName = newItemTextField.text, newItemName != ""{
+            let newItem = Item(name: newItemName)
+            foodNeeds.append(newItem)
+            tableView.reloadData()
+            
+        }
     }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return foodNeeds.count
     }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        // let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")! but can crash
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") {
+            let itemName = foodNeeds[indexPath.row].name
+            let itemNumber = foodNeeds[indexPath.row].quantity
+            cell.textLabel?.text = itemName
+            cell.detailTextLabel?.text = "Quantity: \(itemNumber)"
+            return cell
+        } else {
+            return UITableViewCell()
+        }
     }
+    
 }
 
